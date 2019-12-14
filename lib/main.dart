5f668a7c130deeb47ee23d1bc30fa5e93791a786
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-14 16:38:35 -0300

    Flutter Card & ListTile Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,22 @@ class MyApp extends StatelessWidget {
     @required IconData icon,
     @required String text,
   }) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(10.0),
+    return Card(
       margin: EdgeInsets.symmetric(
         vertical: 10.0,
         horizontal: 25.0,
       ),
-      child: Row(
-        children: <Widget>[
-          Icon(
-            icon,
-            color: Colors.teal,
+      child: ListTile(
+        title: Text(
+          text,
+          style: TextStyle(
+            color: Colors.teal[900],
           ),
-          SizedBox(
-            width: 10.0,
-          ),
-          Text(
-            text,
-            style: TextStyle(
-              color: Colors.teal[900],
-            ),
-          ),
-        ],
+        ),
+        leading: Icon(
+          icon,
+          color: Colors.teal,
+        ),
       ),
     );
   }
@@ -40,36 +33,45 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: NetworkImage(
-                'https://avatars1.githubusercontent.com/u/47613084?s=460&v=4',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: NetworkImage(
+                  'https://avatars1.githubusercontent.com/u/47613084?s=460&v=4',
+                ),
               ),
-            ),
-            Text(
-              'Eliseu Videira',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontFamily: 'Permanent Marker',
-                color: Colors.white,
+              Text(
+                'Eliseu Videira',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontFamily: 'Permanent Marker',
+                  color: Colors.white,
+                ),
               ),
-            ),
-            Text(
-              'Flutter Developer'.toUpperCase(),
-              style: TextStyle(
-                color: Colors.teal[100],
-                letterSpacing: 4.0,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-                fontFamily: 'Source Sans Pro',
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  letterSpacing: 4.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                ),
               ),
-            ),
-            _buildCard(icon: Icons.phone, text: '+44 123 456 789'),
-            _buildCard(icon: Icons.email, text: 'myemail@gmail.com')
-          ],
-        )),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              _buildCard(icon: Icons.phone, text: '+44 123 456 789'),
+              _buildCard(icon: Icons.email, text: 'myemail@gmail.com')
+            ],
+          ),
+        ),
       ),
     );
   }
